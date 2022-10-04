c9d9259aeb1336ea32594cf9c644a5c1e01f7474
IsaiasRlc <isaias.carvalho@sptech.school>
2022-10-04 15:40:21 -0300

    Atualizacao de BD

--- Tabelas Banco de Dados/SPRINT-2.sql
+++ Tabelas Banco de Dados/SPRINT-2.sql
@@ -1,5 +1,6 @@
 
 create database EMPRESA;
+
 use EMPRESA;
 
 -- CRIANDO A TABELA DE CADASTRO
@@ -13,7 +14,9 @@ constraint chkemail check (email like '%@%')
 
 select * from CLIENTE;
 select * from PLANTAÇAO;
-select * from ARDUINO;
+select * from SENSOR;
+select * from dados;
+;
 
 -- INSERINDO DADOS
 insert into CLIENTE (nome, email, senha) values
@@ -23,8 +26,8 @@ insert into CLIENTE (nome, email, senha) values
 ('Jefferson Araujo','jefferson.araujo@sptech.school','jeff13');
 
 -- CRIANDO TABELA PLANTAÇÃO
-create table PLANTAÇAO(
-idPlantaçao int auto_increment,
+create table PLANTACAO(
+idPlantacao int auto_increment,
 especie varchar(45),
 estado char(2),
 cidade varchar(45),
@@ -32,12 +35,12 @@ medida varchar(45),
 tamanho int,
 fkCliente int,
 foreign key (fkCliente) references CLIENTE (idCliente),
-primary key (idPlantaçao,fkCliente),
+primary key (idPlantacao,fkCliente),
 constraint chkmedida check (medida = 'm²')
 )auto_increment=10;
 
 -- INSERINDO DADOS 
-insert into PLANTAÇAO values
+insert into PLANTACAO values
 (null,'Arábica','MG','Carmo de Minas','m²',2, 100),
 (null,'Robusta','MT','Juína','m²',3, 101),
 (null,'Eugenioides','SP','Joanópolis','m²',10, 102),
@@ -45,31 +48,45 @@ insert into PLANTAÇAO values
 (null,'Robusta','SP','Campinas','m²',5, 101),
 (null,'Eugenioides','SP','Ribeirão Preto','m²',4, 102);
 
--- CRIANDO TABELA ARDUINO
-create table ARDUINO(
-idControle int ,
-temperatura decimal(4,2),
-umidade varchar(3),
-momento datetime default current_timestamp,
-fkPlantaçao int auto_increment,
-foreign key (fkPlantaçao) references PLANTAÇAO (idPlantaçao),
-primary key (idControle,fkPlantaçao)
-);
-
+-- CRIANDO TABELA SENSOR
+create table SENSOR(
+idSensor int primary key auto_increment,
+Tipo varchar (10),
+fkPlantacao int,
+foreign key (fkPlantacao) references PLANTACAO (idPlantacao)
+)auto_increment=500;
 
 -- INSERINDO DADOS
-insert into ARDUINO (idControle, temperatura, umidade, fkPlantaçao)values
-(1, '19.20', '10%', 10),
-(2, 20.15, '10%',11),
-(3, 21.45, '11%', 12),
-(4, 18.00, '12%', 13),
-(5, 22.00, '10%', 14),
-(6, 18.55, '11%', 15);
+insert into SENSOR values
+(null, 'DHT-11',10),
+(null,'DHT-11',11),
+(null, 'DHT-11', 12),
+(null,'DHT-11' , 13),
+(null,'DHT-11' , 14),
+(null,'DHT-11' , 15);
 
+-- CRIANDO TABELA DADOS
+create table DADOS(
+idDados int auto_increment,
+Temperatura decimal (4,2),
+Umidade char (6),
+DatasHoras datetime default CURRENT_TIMESTAMP,
+fkSensor int,
+foreign key (fkSensor) references SENSOR (idSensor),
+primary key (idDados,fkSensor)
+);
+
+insert into DADOS (Temperatura, Umidade, fkSensor) values
+	(21.5, '10%', 500),
+	(20.0, '50%', 501),
+	(22.0, '80%', 500),
+	(19.5, '20%', 500);
+    
 select * from CLIENTE join PLANTAÇAO
 on idCliente = fkCliente;
 
-select * from CLIENTE join PLANTAÇAO
+select * from CLIENTE join PLANTACAO
 on idCliente = fkCliente
-join ARDUINO on idPlantaçao = fkPlantaçao;
+join SENSOR on idPlantacao = fkPlantacao
+join DADOS on idSensor = fkSensor;
 
