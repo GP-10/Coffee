31ac69b749883df5458ea33ab63cc27ff559d5d9
Jefferson-Araujo-Silva <jefferson.silva@sptech.school>
2022-12-04 11:18:39 -0300

    contectando api na tela de cards

--- Site Estático com api/Site Estático/srcjs/models/medidaModel.js
+++ Site Estático com api/Site Estático/srcjs/models/medidaModel.js
@@ -24,12 +24,42 @@ function buscarUltimasMedidas(idDados, limite_linhas) {
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+function buscarMedidasEmTempoReal(idDados) {
 
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
         instrucaoSql = `select top 1
+        temperatura as temperatura, 
+        umidade as umidade,  
+                        CONVERT(varchar, dataHora, 108) as momento_grafico, 
+                        fkSensor
+                        from dados where fksensor = 2
+                    order by idDados desc`;
+
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select 
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,
+                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
+                        fk_aquario 
+                        from medida where fk_aquario = ${idAquario} 
+                    order by id desc limit 1`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarMedidasEmTemporealCards(idDados) {
+
+    instrucaoSql = ''
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top 4
         temperatura as temperatura, 
         umidade as umidade,  
                         CONVERT(varchar, dataHora, 108) as momento_grafico, 
